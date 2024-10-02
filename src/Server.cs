aacaec0ce86afebd67bb138031ace6c87e52666a
root <root@ElNagger>
2024-10-02 09:16:48 +0300

    add rdcommand

--- src/Server.cs
+++ src/Server.cs
@@ -11,6 +11,7 @@ var HandleClientAsync = new Action<Socket>(
     async (Socket socket) =>
     {
         var recvStr = "";
+        var rdCmd = new RdCommand(0, new());
         while (true)
         {
             try
@@ -23,6 +24,7 @@ var HandleClientAsync = new Action<Socket>(
             {
                 Console.WriteLine(e);
                 recvStr = "";
+                rdCmd = new RdCommand(0, new());
             }
         }
     }
@@ -31,4 +33,33 @@ while (true)
 {
     var socket = server.AcceptSocket();
     HandleClientAsync(socket);
+}
+
+public class RdCommand
+{
+    public int size { get; set; }
+    public List<RdCommandArg> args { get; set; }
+
+    public RdCommand(int size, List<RdCommandArg> args)
+    {
+        this.size = size;
+        this.args = args;
+    }
+
+    public void Print()
+    {
+        var s = new StringBuilder();
+        foreach (var arg in args)
+        {
+            s.Append(arg.Data);
+            s.Append(" ");
+        }
+        Console.WriteLine(s);
+    }
+}
+
+public class RdCommandArg
+{
+    public int Length { get; set; }
+    public string Data { get; set; }
 }
